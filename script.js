a78bb7c94605bf02116737b9c6071ce4e81f1bfd
UZD007 <djdh9515@gmail.com>
2025-04-15 13:18:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let ism=prompt("String typeda malumot kiriting");
+let num=+prompt("Number typeda malumot kiriting");
+let bol=prompt("Boolean typeda malumot kiriting");
+confirm("Hammasi to'g'rimi")
+
+alert(`Siz kiritgan ${ism} so'zi ${typeof ism}  typeda \n Siz kiritgan ${num} raqami  ${typeof num} typeda \n Siz kiritgan ${bol} so'zi ${typeof bol} typeda`)
+
+
+
+
+
+
